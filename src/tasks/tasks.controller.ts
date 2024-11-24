078d186b6bdb8802790ae6012eb8cbdf57b5fdb1
Petersonwei <140482566+Petersonwei@users.noreply.github.com>
2024-11-24 22:29:17 +1000

    Validating updateTaskStatus Method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -16,7 +17,7 @@ export class TasksController {
       return this.tasksService.getTasksWithFilters(filterDto);
     }
     return this.tasksService.getAllTasks();
-}
+  }
 
   @Get(':id')
   getTaskById(@Param('id') id: string): Task{
@@ -35,8 +36,9 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
   ): Task {
+    const { status } = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
   }
 
